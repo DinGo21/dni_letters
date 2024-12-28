906c3cd8685a2550595349af42014f0f90711e81
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-12-28 19:38:44 +0100

    feat: added static function find to Letter

--- src/Models/Letter.php
+++ src/Models/Letter.php
@@ -10,13 +10,30 @@ class Letter
 
 	private readonly ?int $id;
 	private string        $letter;
-	private string        $table;
 
 	public function __construct(string $letter = "", ?int $id = null)
 	{
 		$this->db = new Database("letters");
-		$this->table = "letters";
 		$this->id = $id;
 		$this->letter = $letter;
 	}
-}
+
+	public function getId() : int
+	{
+		return $this->id;
+	}
+
+	public function getLetter() : string
+	{
+		return $this->letter;
+	}
+
+	public static function find(int $id) : Letter
+	{
+		$db = new Database("letters");
+		$query = $db->mysql->query("SELECT * FROM letters WHERE `id` = {$id}");
+		$result = $query->fetchAll();
+
+		return new Letter($result[0]["letter"], $result[0]["id"]);
+	}
+};
